95da8a3921e179a87d85aab692342b4ea93d949e
Pavel Potoplyak <ppotoplyak@gmail.com>
2015-04-17 19:21:54 -0500

    add example output

--- read_serial_lab9.py
+++ read_serial_lab9.py
@@ -1,7 +1,8 @@
 import serial
 # for windows replace '/dev/ttyACM0' with something like 'COM3', see http://pyserial.sourceforge.net/pyserial_api.html
-# note the baud is 115200
 # needs super user rights to open '/dev/ttyACM0', run like 'sudo python read_serial_lab9.py'
+# on micro, set UART#_IBRD_R = 43 and UART#_FBRD_R = 26 to match 115200 baud
+# on micro write to UART0, unless you have one of these http://amzn.com/B008AGDTA4
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False, bytesize=8, parity='N', stopbits=1)
 good_msg_count = 0
 
