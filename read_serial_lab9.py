5ab335cac534685bd40e2d6aaa8d1e2cc5328828
Pavel Potoplyak <ppotoplyak@gmail.com>
2015-04-22 18:56:07 -0500

    add echo ability

--- read_serial_lab9.py
+++ read_serial_lab9.py
@@ -3,44 +3,50 @@ import serial
 # needs super user rights to open '/dev/ttyACM0', run like 'sudo python read_serial_lab9.py'
 # on micro, set UART#_IBRD_R = 43 and UART#_FBRD_R = 26 to match 115200 baud
 # on micro write to UART0, unless you have one of these http://amzn.com/B008AGDTA4
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False, bytesize=8, parity='N', stopbits=1)
+ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False, bytesize=8, parity='N', stopbits=1)
 good_msg_count = 0
+echo_bytes = True
+
+def get_next_char():
+    next_char = ser.read(1)[0]
+    if echo_bytes:
+        ser.write(next_char)
+    return next_char
 
 def read_until_start_byte():
     while True:
-        data = ser.read(1)
-        if data[0] != chr(0x02):
-            print("Ignoring 0x%X while waiting for start byte"  % ord(data[0]))
+        char_read = get_next_char()
+        if char_read != chr(0x02):
+            print("Ignoring 0x%X while waiting for start byte"  % ord(char_read))
         else:
-            return
+            return char_read
 
 while True:
-    read_until_start_byte()
-    msg = [chr(0x02), ser.read(1)[0]]
+    msg = [read_until_start_byte(), get_next_char()]
     if not msg[len(msg) - 1].isdigit():
         print("Bad byte %d: %X"  % (len(msg) - 1,  ord(msg[len(msg) - 1])))
         continue
-    msg.append(ser.read(1)[0])
+    msg.append(get_next_char())
     if msg[len(msg) - 1] != '.':
         print("Bad byte %d: 0x%X"  % (len(msg) - 1,  ord(msg[len(msg) - 1])))
         continue
-    msg.append(ser.read(1)[0])
+    msg.append(get_next_char())
     if not msg[len(msg) - 1].isdigit():
         print("Bad byte %d: 0x%X"  % (len(msg) - 1,  ord(msg[len(msg) - 1])))
         continue
-    msg.append(ser.read(1)[0])
+    msg.append(get_next_char())
     if not msg[len(msg) - 1].isdigit():
         print("Bad byte %d: 0x%X"  % (len(msg) - 1,  ord(msg[len(msg) - 1])))
         continue
-    msg.append(ser.read(1)[0])
+    msg.append(get_next_char())
     if not msg[len(msg) - 1].isdigit():
         print("Bad byte %d: 0x%X"  % (len(msg) - 1,  ord(msg[len(msg) - 1])))
         continue
-    msg.append(ser.read(1)[0])
+    msg.append(get_next_char())
     if msg[len(msg) - 1] != chr(0x0D):
         print("Bad byte %d: 0x%X"  % (len(msg) - 1,  ord(msg[len(msg) - 1])))
         continue
-    msg.append(ser.read(1)[0])
+    msg.append(get_next_char())
     if msg[len(msg) - 1] != chr(0x03):
         print("Bad byte %d: 0x%X"  % (len(msg) - 1,  ord(msg[len(msg) - 1])))
         continue
